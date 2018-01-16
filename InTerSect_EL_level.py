92fd1f1227254133d86aa02d9d9b561e5af913a0
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2018-01-16 12:13:06 +0100

    Add files via upload

--- InTerSect_EL_level.py
+++ InTerSect_EL_level.py
@@ -341,7 +341,7 @@ print 'sol_ H_I(P) - H_II(P)  =', sol
 evalf_result_c = [complex(x.evalf()) for x in sol]
 print 'evalf_result_c = ', evalf_result_c
 
-filtered = [i for i in evalf_result_c if i.imag == 0]
+filtered = [i for i in evalf_result_c if i.imag < 1E-10]
 print 'filtered = ', filtered
 
 real_roots = [i.real for i in filtered]
@@ -362,6 +362,10 @@ print 'real_roots_zero_to_four = ', real_roots_zero_to_four
 P_real_intersection = real_roots_zero_to_four[0]
 H_real_intersection = z_I(real_roots_zero_to_four[0])
 
+T_folder = 0
+output_array_2 = np.vstack((T_folder, P_real_intersection, H_real_intersection)).T
+np.savetxt('P_H_real_intersection.dat', output_array_2, header="Temperature (K) \t Pressure_Intersection (GPa) \t H_Intersection = E + PV (a.u.)", fmt="%0.13f")
+
 plt.xlabel(r'$P$ (GPa)', fontsize=20)
 plt.ylabel(r'$(H = E + PV)$ / F.U. (a.u.)', fontsize=15)
 plt.suptitle("PBE-D3, pob-TZVP, SHRINK 8 8, Bipolar 18 18, TOLINTEG 8 18, XXLGRID, TOLDEE 8")
@@ -373,5 +377,5 @@ ax.annotate('Analytic\nIntersection\nP= %g GPa\nG = %g a.u.' %(P_real_intersecti
             )
 plt.savefig('calcite_I_and_II_all_2_summary_better_plot_delta_H_exact_expression_with_intersection.pdf', bbox_inches='tight')
 
-plt.show()
+#plt.show()
 
